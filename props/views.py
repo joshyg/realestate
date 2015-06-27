20403ecbb9a6297ebb756a2b292c059ec34402c5
joshyg <joshgrossberg@gmail.com>
2015-06-27 17:38:43 +0000

    added roi data, support for nighborhood/city queries, code refactoring

--- props/views.py
+++ props/views.py
@@ -13,6 +13,7 @@ import math, random
 import time,datetime
 import sys
 import mongoengine
+import re
 from props.models import *
 
 #mongodb
@@ -24,8 +25,116 @@ from props.models import *
 #
 #convert month/year into index in our response['sales'] array
 #
+
+state_abbrevs = ['al','ak','az','ar','ca','co','ct','de','fl','ga','hi','id','il','in','ia','ks','ky','la','me','md','ma','mi','mn','ms','mo','mt','ne','nv','nh','nj','nm','ny','nc','nd','oh','ok','or','pa','ri','sc','sd','tn','tx','ut','vt','va','wa','wv','wi','wy']
+states = ['alabama','alaska','arizona','arkansas','california','colorado','connecticut','delaware','florida','georgia','hawaii','idaho','illinois','indiana','iowa','kansas','kentucky','louisiana','maine','maryland','massachusetts','michigan','minnesota','mississippi','missouri','montana','nebraska','nevada','new hampshire','new jersey','new mexico','new york','north carolina','noth dakota','ohio','oklahoma','oregon','pennsylvania','rhode island','south carolina','south dakota','tennessee','texas','utah','vermont','virginia','washington','west virginia','wisconsin','wyoming']
+
+class QueryTracker(object):
+
+    def __init__( self, request ):
+        self.request = request
+        self.response = {}
+        self.query_dict = {}
+
+    def initSalesList( self ):
+        print 'in initSalesList'
+        self.response['sales'] = []
+        now = str(datetime.datetime.now()).split('-')
+        current_yr=int(now[0])
+        current_month=int(now[1])
+        print 'current period = %d/%d'%(current_month, current_yr)
+        for yr in range(1990,current_yr + 1):
+            for month in range(1,13):
+                if ( current_yr <= yr and current_month < month ):
+                    break
+                self.response['sales'].append({ 'period': '%d-%d'%(yr,month), 'count': 0, 'total_price': 0, 'avg_price': 0, 'roi' : 0 })
+        print self.response['sales'][-1]
+
+    def populateSalesList( self ):
+        print 'about to iterate over properties'
+        for property in self.properties:
+            for sale in property.sales:
+                print sale
+                if ( sale.get('date','') != '' and sale.get('price', 0) != 0 ):
+                    self.response['sales'][periodToIndex(sale['year'], sale['month'])]['total_price'] += sale['price']
+                    self.response['sales'][periodToIndex(sale['year'], sale['month'])]['count'] += 1
+        print 'about to calculate avg and roi'
+        for period in range( len( self.response['sales'] ) ):
+            if ( self.response['sales'][period]['count'] != 0 ):
+                self.response['sales'][period]['avg_price'] = self.response['sales'][period]['total_price']/self.response['sales'][period]['count']
+                # roi calculation begins 1 after the first period with sales.
+                # the first period with sales will always have roi = 1
+                if ( period == 0 or self.response['sales'][period-1]['roi'] == 0 or self.response['sales'][period-1]['avg_price'] == 0 ):
+                    self.response['sales'][period]['roi'] = 1
+                else:
+                    self.response['sales'][period]['roi'] = 1.0*self.response['sales'][period-1]['roi'] * self.response['sales'][period]['avg_price'] / self.response['sales'][period-1]['avg_price']
+
+    def isState( self, str ):
+        if ( str in state_abbrevs or str in states ):
+            return True
+        return False
+
+    def parseRequest( self, search_term ):
+        print 'in parseRequest'
+        #zip code?
+        search_re = re.search( '^ *(\d+) *$', search_term )
+        if ( search_re ):
+            self.query_dict['zip'] = int(search_re.group(1))
+            self.response['header'] = self.query_dict['zip']
+            print 'zip = %s'%self.query_dict['zip']
+            return
+            
+        # compound?  could be city, state or neighborhood, city
+        search_re = re.search( '^ *(.*),\s+(.*) *$', search_term )
+        if ( search_re ):
+            if ( self.isState( str(search_re.group(1).lower() ) ) ):
+                self.query_dict['city'] = int(search_re.group(1))
+                self.query_dict['state'] = int(search_re.group(2))
+                self.response['header'] = self.query_dict['city']
+                print 'query for city %s'%self.query_dict['city']
+            else:
+                self.query_dict['neighborhood'] = int(search_re.group(1))
+                self.query_dict['city'] = int(search_re.group(2))
+                self.response['header'] = self.query_dict['neighborhood']
+                print 'query for neighborhood %s'%self.query_dict['neighborhood']
+            return
+                
+        # no comma? This will have to be improved, but for now, we look for if its a state,
+        # then we see if we have a city in the db with this name, then county, then neighborhood
+        # there are obvious caes where there could be both (la, new york counties), leaving as a FIXME
+        search_re = re.search( '^ *(.*) *$', search_term )
+        if ( search_re ):
+            search_str = str(search_re.group(1).lower()) 
+            if ( self.isState( search_str ) ):
+                self.query_dict['state'] = seacrh_str
+                self.response['header'] = self.query_dict['state']
+                print 'query for state %s'%self.query_dict['state']
+            elif( Properties.objects.filter( city = search_str ).count() > 0 ):
+                self.query_dict['city'] = str(search_re.group(1))
+                self.response['header'] = self.query_dict['city']
+                print 'query for city %s'%self.query_dict['city']
+            elif( Properties.objects.filter( county = search_str ).count() > 0 ):
+                self.query_dict['county'] = str(search_re.group(1))
+                self.response['header'] = self.query_dict['county']
+                print 'query for county %s'%self.query_dict['county']
+            elif( Properties.objects.filter( neighborhood = search_str ).count() > 0 ):
+                print 'query for neighborhood %s'%self.query_dict['neighborhood']
+                self.query_dict['neighborhood'] = str(search_re.group(1))
+                self.response['header'] = self.query_dict['neighborhood']
+
+    def filterProperties( self ):
+        print 'in filterProperties'
+        self.parseRequest( self.request.GET.get( 'search_term', '' ) )
+    
+        if ( self.query_dict != {} ):
+            self.properties = Properties.objects.filter( **self.query_dict )
+            print '%d results'%self.properties.count()
+        else:
+            print 'no filter!'
+            self.properties = []
+
 def periodToIndex( year, month ):
-    return 12*(year - 1990) + month
+    return 12*(year - 1990) + month - 1
 
 def main(request={}):
     csrf_request = {}
@@ -34,31 +143,15 @@ def main(request={}):
 
 def search(request):
     print 'in views.search'
-    response = {}
-    response['sales'] = []
 
-    for yr in range(1990,2016):
-        for month in range(1,13):
-            response['sales'].append({ 'period': '%d-%d'%(yr,month), 'count': 0, 'total_price': 0, 'avg_price': 0 })
+    qt = QueryTracker(request)
 
-    zipcode = int(request.GET.get( 'zip', 0 ))
-    # send back the zipcode from the request
-    response['zip'] = zipcode
-    print 'zip = %s'%zipcode
-
-    properties = Properties.objects.filter( zip = zipcode )  
-    print 'about to iterate over properties'
-    for property in properties:
-        for sale in property.sales:
-            if ( sale.get('date','') != '' and sale.get('price', 0) != 0 ):
-                response['sales'][periodToIndex(sale['year'], sale['month'])]['total_price'] += sale['price']
-                response['sales'][periodToIndex(sale['year'], sale['month'])]['count'] += 1
-    for period in range( len( response['sales'] ) ):
-        if ( response['sales'][period]['count'] != 0 ):
-            response['sales'][period]['avg_price'] = response['sales'][period]['total_price']/response['sales'][period]['count']
+    qt.initSalesList()
+    qt.filterProperties()
+    qt.populateSalesList()
 
     print 'done iterating  over filtered properties'
-    json_str = json.dumps(response)
+    json_str = json.dumps(qt.response)
     print 'converrted to json'
     if ( sys.version_info > (2, 7) ):
         return HttpResponse(json_str, content_type='application/json')
--- templates/main.js
+++ templates/main.js
@@ -1,14 +1,17 @@
 var serverdata;
 sales_array = new Array();;
+roi_array = new Array();;
 var num_datasets = 0;
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(draw_chart);
 
 function init_page() {
     sales_array[0] = ['Date']
+    roi_array[0] = ['Date']
     for ( var year = 1990; year <= 2015; year++ ) {
         for ( var month = 1; month <= 12; month++ ) {
             sales_array.push( [ month+'/'+year ] );
+            roi_array.push( [ month+'/'+year ] );
         }
     }
 }
@@ -19,12 +22,16 @@ function draw_chart() {
     console.log(sales_array);
     if ( serverdata ) {
         var sales_array_index=0; 
-        sales_array[0].push( new String ( serverdata.zip ) );
+        sales_array[0].push( new String ( serverdata.header ) );
+        roi_array[0].push( new String ( serverdata.header ) );
         for (i in serverdata.sales) {
             sales_array_index += 1; 
             console.log( sales_array[sales_array_index] );
             sales_array[sales_array_index].push( serverdata.sales[i].avg_price );
+            roi_array[sales_array_index].push( serverdata.sales[i].roi );
         }  
+        sales_array = sales_array.slice(0, sales_array_index+1);
+        roi_array   = roi_array.slice(0, sales_array_index+1);
         console.log('sales_array')
         console.log(sales_array);
         var data = google.visualization.arrayToDataTable( sales_array );
@@ -40,6 +47,21 @@ function draw_chart() {
       
         if ( serverdata.sales.length >= 1 ){
             var chart = new google.visualization.LineChart(document.getElementById('main_chart'));
+            chart.draw(data, options);
+        }
+        
+        data = google.visualization.arrayToDataTable( roi_array );
+
+        options = {
+          title: 'ROI vs. Time',
+          hAxis: {title: 'Sales'},
+          vAxis: {title: 'ROI'},
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        if ( serverdata.sales.length >= 1 ){
+            var chart = new google.visualization.LineChart(document.getElementById('roi_chart'));
             chart.draw(data, options);
         }
     }
@@ -58,7 +80,7 @@ function ajax_submit(){
     }
 
     console.log('sending...');
-    str = 'zip='+document.forms['main_search']['zip'].value;
+    str = 'search_term='+document.forms['main_search']['search_term'].value;
     req.open('GET', '/search/?'+str, true);
     req.send();
 }
