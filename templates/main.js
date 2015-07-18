7805c8fff36f0a9975adc4191ee828f91837873f
joshyg <joshgrossberg@gmail.com>
2015-07-18 15:42:05 +0000

    removed gap between filter and results, added clear function, fixed select box

--- templates/main.js
+++ templates/main.js
@@ -6,15 +6,24 @@ advanced_search= 0;
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(draw_chart);
 
+data_type_array = [ 
+    'Median Sale Price All Homes',
+    'Median Price/Rent ratio All Homes',
+    'Median Price Per Square Foot All Homes',
+]
+
 function init_page() {
     sales_array[0] = ['Date']
     roi_array[0] = ['Date']
 }
 
-function draw_chart() {
+function draw_chart( ) {
     console.log('in draw chart');
     console.log('initial sales_array')
-    if ( serverdata ) {
+    document.getElementById('main_chart').style.display = 'block';
+    document.getElementById('roi_chart').style.display = 'block';
+    console.log(clear_chart);
+    if ( serverdata.sales.length >= 1 ){
         var sales_array_index=0; 
         sales_array[0].push( String ( serverdata.header ) );
         roi_array[0].push( String ( serverdata.header ) );
@@ -23,21 +32,21 @@ function draw_chart() {
             if ( sales_array.length <= sales_array_index ) {
                 sales_array.push( [ serverdata.sales[i].period, serverdata.sales[i].avg_price ] );
                 roi_array.push(   [ serverdata.sales[i].period, serverdata.sales[i].roi ] );
-            }  
+            }
             else {
                 sales_array[sales_array_index].push( serverdata.sales[i].avg_price );
                 roi_array[sales_array_index].push(   serverdata.sales[i].roi );
-            }  
-        }  
+            }
+        }
         sales_array = sales_array.slice(0, sales_array_index+1);
         roi_array   = roi_array.slice(0, sales_array_index+1);
-        console.log('sales_array')
+        console.log('sales_array');
         console.log(sales_array);
-        var data = google.visualization.arrayToDataTable( sales_array );
+        sales_data = google.visualization.arrayToDataTable( sales_array );
 
         
         var options = {
-          title: 'Sales vs. Time',
+          title: data_type_array[ document.forms['main_search']['data_type'].value ],
           hAxis: {title: 'Dates'},
           vAxis: {title: 'Price'},
           curveType: 'function',
@@ -45,14 +54,15 @@ function draw_chart() {
         };
       
         if ( serverdata.sales.length >= 1 ){
-            var chart = new google.visualization.LineChart(document.getElementById('main_chart'));
-            chart.draw(data, options);
+            console.log('drawing sales chart');
+            var sales_chart = new google.visualization.LineChart(document.getElementById('main_chart'));
+            sales_chart.draw(sales_data, options);
         }
         
-        data = google.visualization.arrayToDataTable( roi_array );
+        roi_data = google.visualization.arrayToDataTable( roi_array );
 
         options = {
-          title: 'ROI vs. Time',
+          title: '% increase vs. Time',
           hAxis: {title: 'Dates'},
           vAxis: {title: 'ROI'},
           curveType: 'function',
@@ -60,8 +70,8 @@ function draw_chart() {
         };
 
         if ( serverdata.sales.length >= 1 ){
-            var chart = new google.visualization.LineChart(document.getElementById('roi_chart'));
-            chart.draw(data, options);
+            var roi_chart = new google.visualization.LineChart(document.getElementById('roi_chart'));
+            roi_chart.draw(roi_data, options);
         }
     }
   
@@ -74,16 +84,14 @@ function ajax_submit(){
         if (req.readyState==4 && req.status==200){
             document.getElementById("main_form").className = "results_form";
             serverdata = eval( '(' + req.responseText + ')');
-            draw_chart()
+            draw_chart();
+            update_warning();
         }
     }
 
     console.log('sending...');
     str = 'search_term='+document.forms['main_search']['search_term'].value;
-    if ( advanced_search == 1 ) {
-        str += '&region_type='+document.forms['main_search']['region_type'].value;
-        str += '&data_type='+document.forms['main_search']['data_type'].value;
-    }
+    str += '&data_type='+document.forms['main_search']['data_type'].value;
     req.open('GET', '/search/?'+str, true);
     req.send();
 }
@@ -106,4 +114,22 @@ function show_advanced_search() {
     search_form += '<option value=2>Median Price Per Sqft Single Family Residence</option>';
     search_form += '</select>';
     document.getElementById('advanced_search').innerHTML = search_form;
+}
+
+function update_warning() {
+    if ( serverdata.warning != '' ) {
+        document.getElementById('warning').innerHTML = '<b>'+serverdata.warning+'</b/>';
+    }
+    else {
+        document.getElementById('warning').innerHTML = '';
+    }
+}
+
+function clear_chart() {
+    sales_array = [ ['Date'] ];
+    roi_array = [ ['Date'] ];
+    serverdata = {};
+    console.log( 'clearing charts' );
+    document.getElementById('main_chart').style.display = "none";
+    document.getElementById('roi_chart').style.display = "none";
 }
