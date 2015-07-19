650b563b68dfb140202f0c92268630ffc85a31cf
joshyg <joshgrossberg@gmail.com>
2015-07-19 05:39:45 +0000

    added clear button, bootstrap-select select box, support for more datasets.  Added bootstrap js script, which was missing before

--- templates/main.js
+++ templates/main.js
@@ -1,45 +1,72 @@
 var serverdata;
-sales_array = new Array();;
-roi_array = new Array();;
+var sales_array = new Array();;
+var pct_change_array = new Array();;
 var num_datasets = 0;
-advanced_search= 0;
+var data_type = 0;
+var advanced_search= 0;
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(draw_chart);
 
 data_type_array = [ 
     'Median Sale Price All Homes',
-    'Median Price/Rent ratio All Homes',
-    'Median Price Per Square Foot All Homes',
+    'Price/Rent ratio All Homes',
+    'Median Price Per Sq Ft All Homes',
+    'Median Price Per Sq ft Condo',
+    'Median Price Per Sq ft Single Family',
+    'Median Rental Price All Homes',
+    'Median Rental Price Per Sq ft All Homes',
+    'Pct Of Homes Increasing In Value',
+    'Pct Of Homes Decreasing In Value',
+    'Ratio Of Homes Sold as Foreclosure',
+    '# Homes for Rent',
+    'Turnover',
+    'Sale Price To List Ratio',
+    'Median Listing Price',
+    'Median Listing Price Per Sq ft',
+    'Pct of listings with Price Reductions'
 ]
 
 function init_page() {
     sales_array[0] = ['Date']
-    roi_array[0] = ['Date']
+    pct_change_array[0] = ['Date']
+}
+
+function init_data_type_select() {
+    var str = '<select name="data_type" class="selectpicker">'
+    for ( i in data_type_array ) {
+        str += '<option value='+i;
+        if ( i == 0 ) {
+            str += ' selected';
+        }
+        str += '>'+data_type_array[i]+'</option>';
+    }
+    str += '</select>'
+    document.getElementById('data_type_select').innerHTML = str;
 }
 
 function draw_chart( ) {
     console.log('in draw chart');
     console.log('initial sales_array')
     document.getElementById('main_chart').style.display = 'block';
-    document.getElementById('roi_chart').style.display = 'block';
+    document.getElementById('pct_change_chart').style.display = 'block';
     console.log(clear_chart);
     if ( serverdata.sales.length >= 1 ){
         var sales_array_index=0; 
         sales_array[0].push( String ( serverdata.header ) );
-        roi_array[0].push( String ( serverdata.header ) );
+        pct_change_array[0].push( String ( serverdata.header ) );
         for (i in serverdata.sales) {
             sales_array_index += 1; 
             if ( sales_array.length <= sales_array_index ) {
                 sales_array.push( [ serverdata.sales[i].period, serverdata.sales[i].avg_price ] );
-                roi_array.push(   [ serverdata.sales[i].period, serverdata.sales[i].roi ] );
+                pct_change_array.push(   [ serverdata.sales[i].period, serverdata.sales[i].pct_change ] );
             }
             else {
                 sales_array[sales_array_index].push( serverdata.sales[i].avg_price );
-                roi_array[sales_array_index].push(   serverdata.sales[i].roi );
+                pct_change_array[sales_array_index].push(   serverdata.sales[i].pct_change );
             }
         }
         sales_array = sales_array.slice(0, sales_array_index+1);
-        roi_array   = roi_array.slice(0, sales_array_index+1);
+        pct_change_array   = pct_change_array.slice(0, sales_array_index+1);
         console.log('sales_array');
         console.log(sales_array);
         sales_data = google.visualization.arrayToDataTable( sales_array );
@@ -59,19 +86,19 @@ function draw_chart( ) {
             sales_chart.draw(sales_data, options);
         }
         
-        roi_data = google.visualization.arrayToDataTable( roi_array );
+        pct_change_data = google.visualization.arrayToDataTable( pct_change_array );
 
         options = {
-          title: '% increase vs. Time',
+          title: 'Increase vs. Time',
           hAxis: {title: 'Dates'},
-          vAxis: {title: 'ROI'},
+          vAxis: {title: 'Increase'},
           curveType: 'function',
           legend: { position: 'bottom' }
         };
 
         if ( serverdata.sales.length >= 1 ){
-            var roi_chart = new google.visualization.LineChart(document.getElementById('roi_chart'));
-            roi_chart.draw(roi_data, options);
+            var pct_change_chart = new google.visualization.LineChart(document.getElementById('pct_change_chart'));
+            pct_change_chart.draw(pct_change_data, options);
         }
     }
   
@@ -85,13 +112,20 @@ function ajax_submit(){
             document.getElementById("main_form").className = "results_form";
             serverdata = eval( '(' + req.responseText + ')');
             draw_chart();
+            modify_search_bar();
             update_warning();
         }
     }
 
+    // If the data type has changed we clear the charts.
+    if ( data_type != document.forms['main_search']['data_type'].value ) {
+        data_type = document.forms['main_search']['data_type'].value;
+        clear_chart();
+    }
+
     console.log('sending...');
     str = 'search_term='+document.forms['main_search']['search_term'].value;
-    str += '&data_type='+document.forms['main_search']['data_type'].value;
+    str += '&data_type='+data_type;
     req.open('GET', '/search/?'+str, true);
     req.send();
 }
@@ -116,6 +150,10 @@ function show_advanced_search() {
     document.getElementById('advanced_search').innerHTML = search_form;
 }
 
+function modify_search_bar() {
+    document.getElementById('clear_button').innerHTML = '<button type="button" class="btn btn-primary btn-large" onclick="clear_chart()" >Clear</button>';
+}
+
 function update_warning() {
     if ( serverdata.warning != '' ) {
         document.getElementById('warning').innerHTML = '<b>'+serverdata.warning+'</b/>';
@@ -127,9 +165,9 @@ function update_warning() {
 
 function clear_chart() {
     sales_array = [ ['Date'] ];
-    roi_array = [ ['Date'] ];
+    pct_change_array = [ ['Date'] ];
     serverdata = {};
     console.log( 'clearing charts' );
     document.getElementById('main_chart').style.display = "none";
-    document.getElementById('roi_chart').style.display = "none";
+    document.getElementById('pct_change_chart').style.display = "none";
 }
